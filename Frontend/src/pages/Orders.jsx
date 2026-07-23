e74fa1d01d0f4bfc9bc536e2084ea1084338a39c
Vinayy31 <vinaykumarsmg31@gmail.com>
2026-07-23 19:12:12 +0530

    Changing files

--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 import "./Orders.css";
 
 function Orders() {
